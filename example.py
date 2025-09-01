a165d6173754576e58bc79a05f97b555cd80363c
jdorantes <165170008+jdorantes1987@users.noreply.github.com>
2025-09-01 08:24:05 -0400

    Actualización

--- example.py
+++ example.py
@@ -2,15 +2,15 @@ import mysql.connector
 
 # Tu código no necesita las credenciales del archivo JSON,
 # solo la información del usuario y la base de datos.
-DB_USER = ""
-DB_NAME = ""
+DB_USER = "jdorantes"
+DB_NAME = "AAPN"
 
 try:
     connection = mysql.connector.connect(
         user=DB_USER,
         # Si tu usuario de la base de datos tiene una contraseña, inclúyela aquí.
         # No confundir con las credenciales de la cuenta de servicio.
-        password="",
+        password="*",
         host="127.0.0.1",
         port=3306,
         database=DB_NAME,
@@ -19,9 +19,9 @@ try:
 
     # Ejemplo de operación: Obtener la versión de la base de datos
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * from tbl_creyente")
-        db_version = cursor.fetchone()
-        print(f"Versión de la base de datos: {db_version[1]}")
+        cursor.execute("SELECT * from tbl_creyentes")
+        db_row = cursor.fetchall()
+        print(f"Datos de la tabla tbl_estados: {db_row}")
 
 except mysql.connector.Error as err:
     print(f"❌ Error al conectar a la base de datos: {err}")
